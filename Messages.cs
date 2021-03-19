b616aec235d18d5041b6bdf2302b2a0601922e25
Tope <castillochristopherjames@gmail.com>
2021-03-19 20:38:24 +0800

    Added Message.cs. This will be where the message handler methods and similar things will be declared.

---
+++ Messages.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using DSharpPlus;
+using DSharpPlus.Entities;
+using DSharpPlus.EventArgs;
+using System.Threading.Tasks;
+using System.Text;
+using LazadaDiscordBot.DiscordAccessFiles;
+
+namespace LazadaDiscordBot
+{
+    class Messages
+    {
+        // Add an `await` somewhere inside this method
+        public async Task MessageHandler(DiscordClient s, MessageCreateEventArgs e)
+        {
+            if (e.Message.Content.Contains("!lazhelp"))
+            {
+                new HelpMessageFormat(e).SendMessage();
+            }
+        }
+    }
+}
